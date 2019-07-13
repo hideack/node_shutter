55dfbbaa904a281f35abf0d3ec4d7469b1339b79
hideaki terai <hideack@pepabo.com>
2019-07-13 09:33:00 +0900

    * Add sample source.

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const fastify = require('fastify')({
   logger: true
 })
 
+const puppeteer = require('puppeteer');
+
 const PORT = process.env.PORT || 5000
 
 // Declare a route
+fastify.get('/', function (request, reply) {
+  (async () => {
+    const url = 'https://www.remp.jp/hello';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({path: 'example.png'});
+    await browser.close();
+
+  })();
+
+  reply.send({"status":"done"});
+});
+
 fastify.get('/ping', function (request, reply) {
   reply.send({"ping":"pong"});
 });
