74e284b0fc007f2b2b24c3b5cb4ceac6be40dcb7
hideaki terai <hideack@pepabo.com>
2019-07-13 12:50:31 +0900

    * Update capture logic.

--- index.js
+++ index.js
@@ -6,26 +6,41 @@ const fastify = require('fastify')({
 
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const PORT = process.env.PORT || 5000
 
 // Declare a route
 fastify.get('/', function (request, reply) {
   (async () => {
-    const url = 'https://www.remp.jp/hello';
-    const browser = await puppeteer.launch({
-      args: [
-        '--enable-font-antialiasing',
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ]
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
+    // request.query.section
+    const url = request.query.url;
+    const sha1 = crypto.createHash('sha1')
+    const fileName = `screenshots/${sha1.update(url).digest('hex')}.png`;
 
-    fs.readFile('example.png', (err, data) => {
+    request.log.info(`Save screenshot : ${fileName}`)
+
+    try {
+      await fs.statSync(fileName);
+      request.log.info(`Cache hit! : ${fileName}`)
+    } catch (e) {
+      const browser = await puppeteer.launch({
+        timeout: 60000,
+        args: [
+          '--enable-font-antialiasing',
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      });
+      const page = await browser.newPage();
+      page.setViewport({width: 1280, height: 1280});
+
+      await page.goto(url);
+      await page.screenshot({path: fileName});
+      await browser.close();
+    }
+
+    await fs.readFile(fileName, (err, data) => {
       reply.header('Content-Type', 'image/png').header('Content-Length', data.length).send(data);
     });
 
